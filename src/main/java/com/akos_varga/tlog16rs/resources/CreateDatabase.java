c98585d794c97e9bd78771dadac108813dfe5bc6
Akos Varga <boxakos@gmail.com>
2018-03-05 13:56:24 +0100

    Add private/ directory to .gitignore. Add liquibase to bulid.gradle dependencies. Update CreateDatabase to use liquibase. Add migrations.xml.

--- src/main/java/com/akos_varga/tlog16rs/resources/CreateDatabase.java
+++ src/main/java/com/akos_varga/tlog16rs/resources/CreateDatabase.java
@@ -6,28 +6,59 @@ import com.avaje.ebean.EbeanServer;
 import com.avaje.ebean.EbeanServerFactory;
 import com.avaje.ebean.config.DataSourceConfig;
 import com.avaje.ebean.config.ServerConfig;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import liquibase.Contexts;
+import liquibase.Liquibase;
+import liquibase.database.DatabaseConnection;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  *
  * @author Akos Varga
  */
+@Slf4j
 @Path("/timelogger/save/test")
 public class CreateDatabase {
     
-    DataSourceConfig sourceConfig;
-    ServerConfig serverConfig;
-    EbeanServer ebeanServer;
+    private DataSourceConfig sourceConfig;
+    private ServerConfig serverConfig;
+    private EbeanServer ebeanServer;
+    private static final String URL = "jdbc:mariadb://127.0.0.1:9001/timelogger";
+    private static final String USERNAME= "timelogger";
+    private static final String PASSWORD = "633Ym2aZ5b9Wtzh4EJc4pANx";
     
     public CreateDatabase(){
+        updateSchema();
         initDataSourceConfig();
         initServerConfing();
         ebeanServer = EbeanServerFactory.create(serverConfig);        
     } 
+    
+    private void updateSchema() {
+        
+        try {
+            DriverManager.registerDriver(new org.mariadb.jdbc.Driver() );
+            
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            DatabaseConnection databaseConnection = new JdbcConnection(connection);
+            Liquibase liquibase = new Liquibase("migrations.xml", new ClassLoaderResourceAccessor(), databaseConnection);
+            liquibase.update(new Contexts());
+        } catch (SQLException | LiquibaseException ex) {
+            log.error(ex.getMessage());
+        }
+                
+        
+    }
     
     private void initDataSourceConfig(){
         sourceConfig = new DataSourceConfig();
@@ -40,8 +71,8 @@ public class CreateDatabase {
     private void initServerConfing(){
         serverConfig = new ServerConfig();
         serverConfig.setName("timelogger");
-        serverConfig.setDdlGenerate(true);
-        serverConfig.setDdlRun(true);
+        serverConfig.setDdlGenerate(false);
+        serverConfig.setDdlRun(false);
         serverConfig.setRegister(true);
         serverConfig.setDataSourceConfig(sourceConfig);
         serverConfig.addClass(TestEntity.class);
